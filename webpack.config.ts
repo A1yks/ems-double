e64ea54485c92e11e0b5a87c593b253ebc0b64d8
A1yks <kuftirev.alex@gmail.com>
2024-06-11 23:20:51 +0300

    implement main layout

--- webpack.config.ts
+++ webpack.config.ts
@@ -25,6 +25,10 @@ const config: webpack.Configuration = {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     plugins: [
