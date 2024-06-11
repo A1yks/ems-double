02f15c2adfce7c8c4bac2b5a4fc34b5b1ef41e70
A1yks <kuftirev.alex@gmail.com>
2024-06-11 20:45:28 +0300

    project set up

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import path from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config: webpack.Configuration = {
+    entry: './src/script.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    devServer: {
+        port: 3000,
+        static: './src',
+    },
+};
+
+export default config;
